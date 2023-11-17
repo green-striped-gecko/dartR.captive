872d531b38d1a33693732012f7011abcec71280b
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2023-11-17 17:21:30 +1100

    added gl.emibd9

--- R/gl.report.parent.offspring.r
+++ R/gl.report.parent.offspring.r
@@ -20,8 +20,8 @@
 #'  [default theme_dartR()].
 #' @param plot_colors List of two color names for the borders and fill of the
 #'  plots [default gl.colors(2)].
-#' @param plot.dir Directory in which to save files [default = working directory]
-#' @param plot.file Name for the RDS binary file to save (base name only, exclude extension) [default NULL]
+#' @param save2tmp If TRUE, saves any ggplots and listings to the session
+#' temporary directory (tempdir) [default FALSE].
 #' @param verbose Verbosity: 0, silent or fatal errors; 1, begin and end; 2,
 #' progress log; 3, progress and results summary; 5, full report
 #' [default 2, unless specified using gl.set.verbosity].
@@ -60,9 +60,14 @@
 #' Note that if your dataset does not contain RepAvg or rdepth among the locus
 #' metrics, the filters for reproducibility and read depth are no used.
 
-#'\strong{ Function's output }
+#' \strong{ Function's output }
 
-#'  Plots and table are saved to the working directory (plot.dir) if plot.file is specified.
+#'  Plots and table are saved to the temporal directory (tempdir) and can be
+#'  accessed with the function \code{\link{gl.print.reports}} and listed with
+#'  the function \code{\link{gl.list.reports}}. Note that they can be accessed
+#'  only in the current R session because tempdir is cleared each time that the
+#'   R session is closed.
+
 #'  Examples of other themes that can be used can be consulted in \itemize{
 #'  \item \url{https://ggplot2.tidyverse.org/reference/ggtheme.html} and \item
 #'  \url{https://yutannihilation.github.io/allYourFigureAreBelongToUs/ggthemes/}
@@ -72,8 +77,9 @@
 #' @author Custodian: Arthur Georges (Post to
 #' \url{https://groups.google.com/d/forum/dartr})
 #' @examples
-#' out <- gl.report.parent.offspring(testset.gl[1:10,1:100])
-#' @seealso \code{\link{gl.report.rdepth}} , \code{\link{gl.report.reproducibility}},
+#' out <- gl.report.parent.offspring(testset.gl[1:10, 1:100])
+#' @seealso \code{\link{gl.list.reports}}, \code{\link{gl.report.rdepth}} ,
+#'  \code{\link{gl.print.reports}},\code{\link{gl.report.reproducibility}},
 #'  \code{\link{gl.filter.parent.offspring}}
 #' @family report functions
 #' @importFrom stats median IQR
@@ -87,204 +93,224 @@ gl.report.parent.offspring <- function(x,
                                        plot.out = TRUE,
                                        plot_theme = theme_dartR(),
                                        plot_colors = gl.colors(2),
-                                       plot.file=NULL,
-                                       plot.dir=NULL,
+                                       save2tmp = FALSE,
                                        verbose = NULL) {
-    # SET VERBOSITY
-    verbose <- gl.check.verbosity(verbose)
-    
-    # SET WORKING DIRECTORY
-    plot.dir <- gl.check.wd(plot.dir,verbose=0)
-    
-    # FLAG SCRIPT START
-    funname <- match.call()[[1]]
-    utils.flag.start(func = funname,
-                     build = "Jody",
-                     verbosity = verbose)
-    
-    # CHECK DATATYPE
-    datatype <- utils.check.datatype(x, verbose = verbose)
-    
-    # DO THE JOB
-    
-    # Generate null expectation for pedigree inconsistency, and outliers
-    if (verbose >= 2) {
-        cat(
-            report(
-                "  Generating null expectation for distribution of counts of 
+  # SET VERBOSITY
+  verbose <- gl.check.verbosity(verbose)
+
+  # FLAG SCRIPT START
+  funname <- match.call()[[1]]
+  utils.flag.start(
+    func = funname,
+    build = "Jody",
+    verbose = verbose
+  )
+
+  # CHECK DATATYPE
+  datatype <- utils.check.datatype(x, verbose = verbose)
+
+  # DO THE JOB
+
+  # Generate null expectation for pedigree inconsistency, and outliers
+  if (verbose >= 2) {
+    cat(
+      report(
+        "  Generating null expectation for distribution of counts of
                 pedigree incompatibility\n"
-            )
-        )
-    }
-    # Assign individuals as populations
-    pop(x) <- x$ind.names
-    # Filter stringently on reproducibility to minimize miscalls
-    if (is.null(x@other$loc.metrics$RepAvg)) {
-        cat(
-            warn(
-                "  Dataset does not include RepAvg among the locus metrics, 
+      )
+    )
+  }
+  # Assign individuals as populations
+  pop(x) <- x$ind.names
+  # Filter stringently on reproducibility to minimize miscalls
+  if (is.null(x@other$loc.metrics$RepAvg)) {
+    cat(
+      warn(
+        "  Dataset does not include RepAvg among the locus metrics,
                 therefore the reproducibility filter was not used\n"
-            )
-        )
-    } else {
-        x <-
-            gl.filter.reproducibility(x, 
-                                      threshold = min.reproducibility,
-                                      verbose = 0)
-    }
-    # Filter stringently on read depth, to further minimize miscalls
-    if (is.null(x@other$loc.metrics$rdepth)) {
-        cat(
-            warn(
-                "  Dataset does not include rdepth among the locus metrics, 
+      )
+    )
+  } else {
+    x <-
+      gl.filter.reproducibility(x,
+        threshold = min.reproducibility,
+        verbose = 0
+      )
+  }
+  # Filter stringently on read depth, to further minimize miscalls
+  if (is.null(x@other$loc.metrics$rdepth)) {
+    cat(
+      warn(
+        "  Dataset does not include rdepth among the locus metrics,
                 therefore the read depth filter was not used\n"
-            )
-        )
-    } else {
-        x <- gl.filter.rdepth(x, lower = min.rdepth, verbose = 0)
-    }
-    
-    # Preliminaries before for loops
-    
-    x2 <- as.matrix(x)
-    split_vectors <- lapply(seq_len(nrow(x2)), function(i)
-        x2[i,])
-    names(split_vectors) <- popNames(x)
-    
-    fun <- function(x, y) {
-        vect <- (x * 10) + y
-        homalts <- sum(vect == 2 | vect == 20, na.rm = T)
-    }
-    
-    count <- sapply(split_vectors, function(vect1) {
-        sapply(split_vectors, function(vect2) {
-            fun(vect1, vect2)
-        })
+      )
+    )
+  } else {
+    x <- gl.filter.rdepth(x, lower = min.rdepth, verbose = 0)
+  }
+
+  # Preliminaries before for loops
+
+  x2 <- as.matrix(x)
+  split_vectors <- lapply(seq_len(nrow(x2)), function(i) {
+    x2[i, ]
+  })
+  names(split_vectors) <- popNames(x)
+
+  fun <- function(x, y) {
+    vect <- (x * 10) + y
+    homalts <- sum(vect == 2 | vect == 20, na.rm = T)
+  }
+
+  count <- sapply(split_vectors, function(vect1) {
+    sapply(split_vectors, function(vect2) {
+      fun(vect1, vect2)
     })
-    
-    counts <- count[lower.tri(count, diag = FALSE)]
-    
-    # Prepare for plotting
-    
-    if (verbose >= 2) {
-        cat(
-            report(
-                "  Identifying outliers with lower than expected counts of 
+  })
+
+  counts <- count[lower.tri(count, diag = FALSE)]
+
+  # Prepare for plotting
+
+  if (verbose >= 2) {
+    cat(
+      report(
+        "  Identifying outliers with lower than expected counts of
                 pedigree inconsistencies\n"
-            )
-        )
-    }
-    title <-
-        paste0("SNP data (DArTSeq)\nCounts of pedigree incompatible loci per 
+      )
+    )
+  }
+  title <-
+    paste0("SNP data (DArTSeq)\nCounts of pedigree incompatible loci per
                pair")
-    
-    counts_plot <- as.data.frame(counts)
-    
-    # Boxplot
-    p1 <-
-        ggplot(counts_plot, aes(y = counts)) + 
-      geom_boxplot(color = plot_colors[1], fill = plot_colors[2]) + 
-      coord_flip() + 
-      plot_theme + 
-      xlim(range = c(-1, 1)) +
-      ylim(min(counts), max(counts)) + 
-      ylab(" ") + 
-      theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + 
-      ggtitle(title)
-    
-    outliers_temp <- ggplot_build(p1)$data[[1]]$outliers[[1]]
-    
-    lower.extremes <-
-        outliers_temp[outliers_temp < stats::median(counts)]
-    if (length(lower.extremes) == 0) {
-        outliers <- NULL
-    } else {
-        outliers <- data.frame(Outlier = lower.extremes)
+
+  counts_plot <- as.data.frame(counts)
+
+  # Boxplot
+  p1 <-
+    ggplot(counts_plot, aes(y = counts)) +
+    geom_boxplot(color = plot_colors[1], fill = plot_colors[2]) +
+    coord_flip() +
+    plot_theme +
+    xlim(range = c(-1, 1)) +
+    ylim(min(counts), max(counts)) +
+    ylab(" ") +
+    theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+    ggtitle(title)
+
+  outliers_temp <- ggplot_build(p1)$data[[1]]$outliers[[1]]
+
+  lower.extremes <-
+    outliers_temp[outliers_temp < stats::median(counts)]
+  if (length(lower.extremes) == 0) {
+    outliers <- NULL
+  } else {
+    outliers <- data.frame(Outlier = lower.extremes)
+  }
+
+  # Ascertain the identity of the pairs
+  if (verbose >= 2) {
+    cat(report("  Identifying outlying pairs\n"))
+  }
+  if (length(lower.extremes) > 0) {
+    tmp <- count
+    tmp[lower.tri(tmp)] <- t(tmp)[lower.tri(tmp)]
+    for (i in 1:length(outliers$Outlier)) {
+      # Identify
+      tmp2 <- tmp[tmp == outliers$Outlier[i]]
+      outliers$ind1[i] <- popNames(x)[!is.na(tmp2)][1]
+      outliers$ind2[i] <- popNames(x)[!is.na(tmp2)][2]
+      # Z-scores
+      zscore <-
+        (mean(count, na.rm = TRUE) - outliers$Outlier[i]) /
+          sd(count, na.rm = TRUE)
+      outliers$zscore[i] <- round(zscore, 2)
+      outliers$p[i] <-
+        round(pnorm(
+          mean = mean(count, na.rm = TRUE),
+          sd = sd(count, na.rm = TRUE),
+          q = outliers$zscore[i]
+        ), 4)
     }
-    
-    # Ascertain the identity of the pairs
-    if (verbose >= 2) {
-        cat(report("  Identifying outlying pairs\n"))
+    # ordering by number of outliers
+    outliers <- outliers[order(outliers, decreasing = T), ]
+    # removing duplicated values
+    outliers <- outliers[!duplicated(outliers), ]
+    # removing NAs
+    outliers <- outliers[complete.cases(outliers), ]
+  }
+
+  # Extract the quantile threshold
+  iqr <- stats::IQR(counts, na.rm = TRUE)
+  qth <- quantile(counts, 0.25, na.rm = TRUE)
+  cutoff <- qth - iqr * range
+
+  # Histogram
+  p2 <-
+    ggplot(counts_plot, aes(x = counts)) +
+    geom_histogram(bins = 50, color = plot_colors[1], fill = plot_colors[2]) +
+    geom_vline(xintercept = cutoff, color = "red", size = 1) +
+    coord_cartesian(xlim = c(min(counts), max(counts))) +
+    xlab("No. Pedigree incompatible") +
+    ylab("Count") +
+    plot_theme
+
+  # Output the outlier loci
+  if (length(lower.extremes) == 0) {
+    cat(important("  No outliers detected\n"))
+  }
+  if (length(lower.extremes) > 0) {
+    outliers <- outliers[order(outliers$Outlier), ]
+    if (verbose >= 3) {
+      print(outliers)
     }
-    if (length(lower.extremes) > 0) {
-        tmp <- count
-        tmp[lower.tri(tmp)] <-t(tmp)[lower.tri(tmp)]
-        for (i in 1:length(outliers$Outlier)) {
-            # Identify
-            tmp2 <- tmp[tmp == outliers$Outlier[i]]
-            outliers$ind1[i] <- popNames(x)[!is.na(tmp2)][1]
-            outliers$ind2[i] <- popNames(x)[!is.na(tmp2)][2]
-            # Z-scores
-            zscore <-
-                (mean(count, na.rm = TRUE) - outliers$Outlier[i]) / 
-              sd(count, na.rm = TRUE)
-            outliers$zscore[i] <- round(zscore, 2)
-            outliers$p[i] <-
-                round(pnorm(
-                    mean = mean(count, na.rm = TRUE),
-                    sd = sd(count, na.rm = TRUE),
-                    q = outliers$zscore[i]
-                ), 4)
-        }
-        # ordering by number of outliers
-        outliers <- outliers[order(outliers, decreasing = T),]
-        # removing duplicated values
-        outliers <- outliers[!duplicated(outliers),]
-        # removing NAs
-        outliers <- outliers[complete.cases(outliers),]
-    }
-    
-    # Extract the quantile threshold
-    iqr <- stats::IQR(counts, na.rm = TRUE)
-    qth <- quantile(counts, 0.25, na.rm = TRUE)
-    cutoff <- qth - iqr * range
-    
-    # Histogram
-    p2 <-
-        ggplot(counts_plot, aes(x = counts)) + 
-      geom_histogram(bins = 50,color = plot_colors[1],fill = plot_colors[2]) + 
-      geom_vline(xintercept = cutoff, color = "red", size = 1) + 
-      coord_cartesian(xlim = c(min(counts), max(counts))) + 
-      xlab("No. Pedigree incompatible") + 
-      ylab("Count") +
-        plot_theme
-    
-    # Output the outlier loci
-    if (length(lower.extremes) == 0) {
-        cat(important("  No outliers detected\n"))
-    }
-    if (length(lower.extremes) > 0) {
-        outliers <- outliers[order(outliers$Outlier),]
-        if (verbose >= 3) {
-            print(outliers)
-        }
-    }
-    
-    df <- outliers
-    # PRINTING OUTPUTS
+  }
+
+  df <- outliers
+  # PRINTING OUTPUTS
+  if (plot.out) {
+    # using package patchwork
+    p3 <- (p1 / p2) + plot_layout(heights = c(1, 4))
+    print(p3)
+  }
+
+  # SAVE INTERMEDIATES TO TEMPDIR
+
+  # creating temp file names
+  if (save2tmp) {
     if (plot.out) {
-        # using package patchwork
-        p3 <- (p1 / p2) + plot_layout(heights = c(1, 4))
-        print(p3)
+      temp_plot <- tempfile(pattern = "Plot_")
+      match_call <-
+        paste0(names(match.call()),
+          "_",
+          as.character(match.call()),
+          collapse = "_"
+        )
+      # saving to tempdir
+      saveRDS(list(match_call, p3), file = temp_plot)
+      if (verbose >= 2) {
+        cat(report("  Saving the ggplot to session tempfile\n"))
+      }
     }
-    
-    # Optionally save the plot ---------------------
-    
-    if(!is.null(plot.file)){
-      tmp <- utils.plot.save(p3,
-                             dir=plot.dir,
-                             file=plot.file,
-                             verbose=verbose)
+    temp_table <- tempfile(pattern = "Table_")
+    saveRDS(list(match_call, df), file = temp_table)
+    if (verbose >= 2) {
+      cat(report("  Saving tabulation to session tempfile\n"))
+      cat(
+        report(
+          "  NOTE: Retrieve output files from tempdir using
+                    gl.list.reports() and gl.print.reports()\n"
+        )
+      )
     }
-    
-    
-    # FLAG SCRIPT END
-    
-    if (verbose >= 1) {
-        cat(report("Completed:", funname, "\n"))
-    }
-    
-    # RETURN
-    invisible(x)
-    
+  }
+
+  # FLAG SCRIPT END
+
+  if (verbose >= 1) {
+    cat(report("Completed:", funname, "\n"))
+  }
+
+  # RETURN
+  invisible(x)
 }
