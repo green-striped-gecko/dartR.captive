b93c313ea841a6b70130ddef0f097f6946b635c8
sam <samuelamini20@gmail.com>
2024-03-11 13:15:47 +1100

    OK

--- R/gl.run.EMIBD9.R
+++ R/gl.run.EMIBD9.R
@@ -294,8 +294,7 @@ inbreeding <- data.frame(as.numeric(tmp_data_raw_3[, 21]))
 
   # PRINTING OUTPUTS
   
- p1 <- heatmap(res, Rowv = NA, Colv = NA)
- p1 + title(main = "Heatmap of Relatedness") 
+ p1 <- heatmap(res, Rowv = NA, Colv = NA, main = "Heatmap of Relatedness") 
  
  p2 <- ggplot(rel.list, aes(x = Relatedness)) +
     geom_histogram(binwidth = 0.01) +
