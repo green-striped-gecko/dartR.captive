bd290a15ce635652c283874541c9ba5ca91b4c1c
Luis Mijangos <luis.mijangos@gmail.com>
2026-02-23 08:57:12 +1100

    fix no visible global function definition for 'coancestry' in utils.functions.diagnostics.relatedness.r

--- R/utils.functions.diagnostics.relatedness.r
+++ R/utils.functions.diagnostics.relatedness.r
@@ -11,7 +11,7 @@ coanct_clean <- function(input, coanctTests = NULL){
   
   test_select[which(tests %in% coanctTests)] <- 1
   
-  x_coancest <- coancestry(x_gl2, trioml = test_select[1], wang = test_select[2],
+  x_coancest <- related::coancestry(x_gl2, trioml = test_select[1], wang = test_select[2],
                            lynchli = test_select[3], lynchrd = test_select[4], ritland = test_select[5], 
                            quellergt = test_select[6], dyadml = test_select[7])
   
